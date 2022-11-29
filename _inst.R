738c68a39ba8a662a8387f259f82d33b151a98cd
Martin J Frigaard <mjfrigaard@gmail.com>
2022-11-28 16:51:12 -0800

    new git slides

--- _inst.R
+++ _inst.R
@@ -1,17 +1,18 @@
-
 pkgs <- c("alphavantager", "anytime", "bookdown", "brio", "crayon", "desc", 
 "DiagrammeR", "DiagrammeRsvg", "distill", "dm", "downlit", "downloader", 
-"duckdb", "forecast", "fracdiff", "ggfittext", "ggplot2", "gridExtra", 
-"gt", "gtrendsR", "hrbrthemes", "igraph", "influenceR", "inspectdf", 
-"ipcwswitch", "jcolors", "jsonlite", "kableExtra", "knitr", "listviewer", 
-"MASS", "Matrix", "metathis", "NHANES", "nlme", "openxlsx", "padr", 
-"PerformanceAnalytics", "plotly", "png", "Quandl", "raster", 
-"RcppRoll", "readr", "rgdal", "riingo", "rsvg", "sass", "shades", 
-"socviz", "starwarsdb", "survival", "tibbletime", "tidycensus", 
-"tidyquant", "tidytuesdayR", "tigris", "timetk", "tsfeatures", 
-"usdata", "V8", "vctrs", "viridis", "visNetwork", "vroom", "webshot", 
-"writexl", "xaringanExtra", "XML", "zipcodeR")
+"duckdb", "flexdashboard", "forecast", "fracdiff", "ggfittext", 
+"ggplot2", "gridExtra", "gt", "gtrendsR", "hrbrthemes", "igraph", 
+"influenceR", "inspectdf", "ipcwswitch", "jcolors", "jsonlite", 
+"kableExtra", "knitr", "listviewer", "lubridate", "MASS", "Matrix", 
+"metathis", "NHANES", "nlme", "openxlsx", "padr", "PerformanceAnalytics", 
+"plotly", "png", "processx", "Quandl", "raster", "RcppRoll", 
+"readr", "rgdal", "riingo", "rsvg", "sass", "shades", "socviz", 
+"starwarsdb", "survival", "terra", "tibbletime", "tidycensus", 
+"tidyquant", "tidytuesdayR", "tidyverse", "tigris", "timechange", 
+"timetk", "tsfeatures", "usdata", "V8", "vctrs", "viridis", "visNetwork", 
+"vroom", "webshot", "writexl", "xaringanExtra", "XML", "zipcodeR"
+)
 
+# unique(sort(pkgs)) |> dput()
 install.packages(pkgs)
-
 # remotes::install_github("kjhealy/covdata@main")
